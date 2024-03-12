0a760c7acc6d25d6bb505c84a86652c3e1bd1358
Trace Cooney <trace.wade.cooney@gmail.com>
2024-03-12 12:18:10 -0600

    Added CodeSnippet.ts

---
+++ src/lib/classes/CodeSnippet.ts
@@ -0,0 +1,8 @@
+export class CodeSnippet {
+    constructor(
+      public title: string,
+      public description: string,
+      public instructions: string,
+      public solution: string
+    ) {}
+  }
